578997087bfb6b3390eec7e6a1124ef3e1704c88
ranim-arfa <ranimarfa6@gmail.com>
2026-03-04 19:49:23 +0100

    Initial project structure: frontend, backend, ai-module

---
+++ backend/smartinvoice-backend/smartinvoice-backend/src/main/java/com/smartinvoice/smartinvoice_backend/SmartinvoiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartinvoice.smartinvoice_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartinvoiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartinvoiceBackendApplication.class, args);
+	}
+
+}
---
+++ backend/smartinvoice-backend/smartinvoice-backend/src/test/java/com/smartinvoice/smartinvoice_backend/SmartinvoiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartinvoice.smartinvoice_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartinvoiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
